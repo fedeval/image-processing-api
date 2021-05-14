5f2126c9d69972b7da1c8ebfc568040af6fed5cb
fedeval <federico.valenza5@gmail.com>
2021-05-14 15:32:00 +0200

    feat: use built-in middleware to serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes/index';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.use('/api', routes);
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
