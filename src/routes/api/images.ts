02e00d965f70fb61603035813233344b26e54bdd
fedeval <federico.valenza5@gmail.com>
2021-05-14 17:39:50 +0200

    feat: api/images endpoint resizes image and saves it into a new file when given filename, height and width query params

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import path from 'path';
+import { resizeImage } from '../../utils/imageTransforms';
+import { promises as fsPromises } from 'fs';
 
 const images = express.Router();
 
-images.get('/', (req, res) => {
-  const filename = req.query.filename;
-  const height = parseInt(req.query.height as unknown as string);
-  const width = parseInt(req.query.width as unknown as string);
-  res.sendFile(path.resolve(`public/images/full/${filename}.jpg`));
+images.get('/', async (req, res) => {
+  try {
+    const filename = req.query.filename as unknown as string;
+    const height = parseInt(req.query.height as unknown as string);
+    const width = parseInt(req.query.width as unknown as string);
+    const resizedImage = await resizeImage(filename, height, width)
+    await fsPromises.writeFile(`public/images/resized/${filename}${height}x${width}.jpg`, resizedImage);
+    res.sendFile(path.resolve(`public/images/resized/${filename}${height}x${width}.jpg`));
+  } catch(err) {
+    console.log(err)
+  }
 });
 
 export default images;
--- src/utils/imageTransforms.ts
+++ src/utils/imageTransforms.ts
@@ -1,18 +1,18 @@
-import sharp from 'sharp';
+import sharp, { OutputInfo, Sharp } from 'sharp';
 import path from 'path';
 
 const resizeImage = (
   filename: string,
   height: number,
   width: number
-): void => {
-  sharp(filename)
+): Promise<Buffer> => {
+  return sharp(path.resolve(`public/images/full/${filename}.jpg`))
     .resize({
       width: width,
       height: height,
       fit: sharp.fit.cover
     })
-    .toFile(path.resolve(`public/images/resize/${filename}${height}x${width}.jpg`));
+    .toBuffer()
 };
 
-export default { resizeImage };
+export { resizeImage };
