d0e8b9d61cd7a6ed6a768e6764ad1e711b057795
fedeval <federico.valenza5@gmail.com>
2021-05-17 23:44:05 +0200

    style: lint and format

--- build/tests/imageTransformsSpec.js
+++ build/tests/imageTransformsSpec.js
@@ -48,4 +48,14 @@ describe('Testing image processing', function () {
             }
         });
     }); });
+    it('Resolves succesfully when provided the right filename, height and width parameters', function () { return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, expectAsync(imageTransforms_1.resizeImage('santamonica', 200, 200)).toBeResolved()];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    }); });
 });
