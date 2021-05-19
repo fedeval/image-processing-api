91a9ab77953a658cc520a2023c8b51368ba95903
fedeval <federico.valenza5@gmail.com>
2021-05-19 23:41:08 +0200

    style: format and lint

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ const port = 3000;
 app.use('/api', routes);
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.set('views', path.join(__dirname, './views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.redirect('/api');
