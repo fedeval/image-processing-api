409489ff232b78f3fd13079d1572c96219dc91bf
fedeval <federico.valenza5@gmail.com>
2021-05-20 22:39:04 +0200

    feat: display raw images on main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import images from './api/images';
+import fs from 'fs';
+import path from 'path';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.render('index', { name: 'Federico' });
+  const basePath = 'public/images/full'
+  const images: string[] = fs.readdirSync(path.resolve(basePath));
+  res.render('index', { images: images, basePath: basePath });
 });
 
 routes.use('/images', images);
