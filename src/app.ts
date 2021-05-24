b54655298a535dbee30b029eb93553632a56743d
fedeval <federico.valenza5@gmail.com>
2021-05-24 22:25:51 +0200

    fix: add type definitions to app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.redirect('/api');
 });
 
