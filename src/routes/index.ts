335fdfb495476f8f5d3f17bfa3fa52cfdc1cd035
fedeval <federico.valenza5@gmail.com>
2021-05-24 21:15:59 +0200

    fix: add type definitions in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', (req: express.Request, res: express.Response) => {
   const imagesFilenames: string[] = fs
     .readdirSync(path.resolve('public/images/full'))
     .map((filename) => filename.slice(0, -4));
